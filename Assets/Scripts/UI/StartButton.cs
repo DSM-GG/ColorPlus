a48082c107391b64139465955034d531e804ca23
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-05-19 21:44:15 +0900

    Add start panel fade effect

---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class StartButton : MonoBehaviour, IPointerClickHandler
+    {
+        // 인스펙터 노출 변수
+        // 일반
+        [SerializeField]
+        private Image coverImage;                  // 커버 이미지
+        
+        // 인스펙터 비노출 변수
+        // 수치
+        private float timer = 0.2f;                // 패널 움직임 속도
+        
+        
+        // 클릭
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            UIManager.instance.MoveUI(0, new Vector2(-1280, 1280), timer);
+
+            StartCoroutine(SetSelectPanel());
+        }
+        
+        // 선택창 UI 켜기
+        private IEnumerator SetSelectPanel()
+        {
+            yield return new WaitForSeconds(timer);
+            
+            UIManager.instance.FadeUI(0, 1, 0, 0.2f);
+
+            coverImage.raycastTarget = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using Board;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager instance;
+		
+        // 인스펙터 노출 변수
+        [SerializeField]
+        private Text[]             texts;				// 텍스트 집합
+        [SerializeField]
+        private GameObject[]       panels;              // ui 집합
+		
+		
+        // 초기화
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+
+        // 텍스트 설정
+        public void SetText(int index, string str)
+        {
+            texts[index].text = str;
+        }
+        
+        // UI 설정
+        public void SetUI(int index, bool enable)
+        {
+            panels[index].gameObject.SetActive(enable);
+        }
+        
+        // 되돌리기 버튼
+        public void RestoreGame()
+        {
+            BoardManager.instance.Restore();
+        }
+        
+        // UI 이동
+        public void MoveUI(int index, Vector2 goalPos, float time)
+        {
+            StartCoroutine(MoveTo(panels[index].GetComponent<RectTransform>(), goalPos, time));
+        }
+        
+        // UI 페이드
+        public void FadeUI(int arrayOffset, int index, float goalAlpha, float time)
+        {
+            switch (arrayOffset)
+            {
+                case 0:
+                    StartCoroutine(Fade(panels[index].GetComponent<Image>(), goalAlpha, time));
+                    
+                    break;
+                case 1:
+                    StartCoroutine(Fade(texts[index], goalAlpha, time));
+                    
+                    break;
+                default:
+                    Debug.Log("arrayOffset");
+                    
+                    break;
+            }
+        }
+
+        // 다시하기 버튼
+        public void ResetGame()
+        {
+            BoardManager.instance.ResetTile();
+        }
+        
+        // 레벨 선택화면으로 돌아가기
+        public void GotoScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        
+        // 이동 코루틴
+        private IEnumerator MoveTo(RectTransform target, Vector2 goalPos, float time)
+        {
+            float originTime = time;
+            Vector2 startPos = target.position;
+            
+           
+            while (time > 0)
+            {
+                target.position = Vector2.Lerp(goalPos, startPos, (time / originTime));
+                
+                time -= 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+        
+        // 페이드 효과 ( 이미지 )
+        private IEnumerator Fade(Image target, float goalAlpha, float time)
+        {
+            float originTime = time;
+            Color originColor = target.color;
+            float startAlpha = target.color.a;
+            
+            
+            while (time > 0)
+            {
+                originColor.a = Mathf.Lerp(goalAlpha, startAlpha, (time / originTime));
+                target.color = originColor;
+                
+                time -= 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+        
+        // 페이드 효과 ( 텍스트 )
+        private IEnumerator Fade(Text target, float goalAlpha, float time)
+        {
+            float originTime = time;
+            Color originColor = target.color;
+            float startAlpha = target.color.a;
+            
+            
+            while (time > 0)
+            {
+                originColor.a = Mathf.Lerp(startAlpha, goalAlpha, (time / originTime));
+                target.color = originColor;
+                
+                time -= 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+}
+
