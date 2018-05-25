4d2005c1f028b2e73236d8298dfdeb7480e1bea2
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-05-25 16:13:37 +0900

    Add option panel index in option button

--- Assets/Scripts/UI/OptionButton.cs
+++ Assets/Scripts/UI/OptionButton.cs
@@ -5,6 +5,11 @@ namespace UI
 {
     public class OptionButton : MonoBehaviour, IPointerClickHandler
     {
+        // 인스펙터 노출 변수
+        // 수치
+        [SerializeField]
+        private int         optionIndex;                // 옵션 패널 인덱스
+        
         // 인스펙터 비노출 변수 
         // 수치
         private static bool enalbed = false;            // 활성화 여부
@@ -15,7 +20,7 @@ namespace UI
         {
             enalbed = !enalbed;
 
-            UIManager.instance.SetUI(0, enalbed);
+            UIManager.instance.SetUI(optionIndex, enalbed);
         }
     }
 }
