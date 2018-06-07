d94cd4e6879eee659cccb59ab4a410b6188148fd
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-06-07 15:02:15 +0900

    Create ZoomInOut script

---
+++ Assets/Scripts/UI/ZoomInOut.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+	public class ZoomInOut : MonoBehaviour
+	{
+		// 매 프레임
+		private void Update()
+		{
+			if (Input.touchCount == 2)
+			{
+				Touch 	touchZero 	= Input.GetTouch(0);
+				Touch 	touchOne 	= Input.GetTouch(1);
+
+				Vector2 touchZeroPrevPos 	= touchZero.position - touchZero.deltaPosition;
+				Vector2 touchOnePrevPos 	= touchOne.position - touchOne.deltaPosition;
+
+				float 	prevTouchDeltaMag 	= (touchZeroPrevPos - touchOnePrevPos).magnitude;
+				float 	touchDeltaMag 		= (touchZero.position - touchOne.position).magnitude;
+
+				float 	deltaMagnitudediff = prevTouchDeltaMag - touchDeltaMag;
+				
+				Debug.Log(deltaMagnitudediff);
+			}
+		}
+	}
+}
